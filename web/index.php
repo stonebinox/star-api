1c2e8183217e9cb1196101f27a1e47164ccadf5a
stonebinox <anoop.santhanam@gmail.com>
2017-10-31 08:11:35 +0530

    Skeleton setup

---
+++ web/index.php
@@ -0,0 +1,35 @@
+<?php
+
+ini_set('display_errors', 1);
+require_once __DIR__.'/../vendor/autoload.php';
+$app = require __DIR__.'/../src/app.php';
+require __DIR__.'/../config/prod.php';
+require __DIR__.'/../src/controllers.php';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr',
+));
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+      'driver' => 'pdo_mysql',
+      'dbname' => 'heroku_d79883d9aa66a0a',
+      'user' => 'b46d8bdcf97357',
+      'password' => '8fdecc7f ',
+      'host'=> "us-cdbr-iron-east-05.cleardb.net",
+    )
+));
+$app->register(new Silex\Provider\SessionServiceProvider, array(
+    'session.storage.save_path' => dirname(__DIR__) . '/tmp/sessions'
+));
+$app->before(function(Request $request) use($app){
+    $request->getSession()->start();
+});
+$app->get("/",function() use($app){
+    $app['twig']->render("index.html.twig");
+});
+$app->run();
+?>
